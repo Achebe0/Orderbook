04ba959026a306ebc14e5b8f0eb5b4fce8f34cce
Achebe <achebeculpepper@gmail.com>
2026-08-27 19:44:49 -0400

    added getters for the Order.h, may add the 3 types of Order Types

--- src/core/order.h
+++ src/core/order.h
@@ -6,12 +6,15 @@
 #define ORDERBOOK_ORDER_H
 
 
-class order {
-};
-
-enum class Orderside {
+enum class OrderSide {
     BUY,
     SELL
+};
+
+enum class OrderType {
+    LIMIT,
+    MARKET,
+    STOP // may add the different types of stop orders in the future
 };
 
 enum class OrderStatus {
@@ -21,4 +24,39 @@ enum class OrderStatus {
     REFUNDED,
     ACTIVE
 };
+
+
+
+class order {
+public:
+    order(int id, OrderSide side, OrderType type, OrderStatus status, double quantity, double price, double remaining_quantity):
+    id(id),
+    side(side),
+    type(type),
+    status(status),
+    quantity(quantity),
+    price(price),
+    remaining_quantity(remaining_quantity){}
+
+
+    int getId() const;
+    OrderSide getSide() const;
+    OrderType getType() const;
+    OrderStatus getStatus() const;
+    double getQuantity() const;
+    double getPrice() const;
+    double getRemainingQuantity() const;
+
+// will use this over a struct for convenience, we can use both but structs are public by default
+private:
+    int id;
+    OrderSide side;
+    OrderType type;
+    OrderStatus status;
+    double quantity;
+    double price;
+    double remaining_quantity;
+
+};
+
 #endif //ORDERBOOK_ORDER_H
